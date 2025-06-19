561ce18cc89cdbf439453481b6c82a095ce6c1b1
Ahmed Khattab <92499066+khattab-dev@users.noreply.github.com>
2025-06-19 13:16:07 +0300

    chore: merging ktor-migration into master
    * Update LocalModule.kt
    
    * Update LocalModule.kt
    
    * Update LocalModule.ktaaa

--- core/di/src/main/java/unilever/it/org/di/LocalModule.kt
+++ core/di/src/main/java/unilever/it/org/di/LocalModule.kt
@@ -3,7 +3,7 @@ package unilever.it.org.di
 import android.content.Context
 import androidx.room.Room
 import dagger.Module
-import dagger.Provides
+import dagger.Provides 
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
@@ -11,11 +11,12 @@ import unilever.it.org.data_source.local.dao.SearchDao
 import unilever.it.org.data_source.local.database.WeatherDatabase
 import javax.inject.Singleton
 
+
 @Module
 @InstallIn(SingletonComponent::class)
 object LocalModule {
     @Provides
-    @Singleton
+    @Singleton 
     fun provideDatabase(@ApplicationContext context: Context): WeatherDatabase {
         return Room.databaseBuilder(
             context,

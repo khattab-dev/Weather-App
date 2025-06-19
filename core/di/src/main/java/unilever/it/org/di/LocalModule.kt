00f45254b87b41207052e811bc288b9d2d55ccb9
Ahmed Khattab <92499066+khattab-dev@users.noreply.github.com>
2025-06-19 13:13:37 +0300

    Update LocalModule.kt

--- core/di/src/main/java/unilever/it/org/di/LocalModule.kt
+++ core/di/src/main/java/unilever/it/org/di/LocalModule.kt
@@ -3,7 +3,7 @@ package unilever.it.org.di
 import android.content.Context
 import androidx.room.Room
 import dagger.Module
-import dagger.Provides
+import dagger.Provides 
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
